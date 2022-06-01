bd6467a9033fef6575370bf1c2642ef2d76441f7
kayodegigz <kayogunrinde@gmail.com>
2022-06-01 17:08:05 +0100

    formatting to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for letter in str:
+        ascii_val = ord(letter)
+        
+        if ascii_val not in range(97, 123):
+            new_str += letter
+        else:
+            ascii_val_upper = ascii_val - 32
+            new_char = chr(ascii_val_upper)
+            new_str += new_char
+    return new_str
+print(uppercase("Best boy"))
