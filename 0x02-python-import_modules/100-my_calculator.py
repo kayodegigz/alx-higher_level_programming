6956e6559a95d72a8ec5406a7e554039af5a7b69
kayodegigz <kayogunrinde@gmail.com>
2022-06-02 23:59:25 +0100

    calculation operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     if sys.argv[2] == "+":
         operation1 = calculator_1.add(first_arg, sec_arg)
         print("{} + {} = {}".format(first_arg, sec_arg, operation))
-    
+
     elif sys.argv[2] == "-":
         operation2 = calculator_1.sub(first_arg, sec_arg)
         print("{} - {} = {}".format(first_arg, sec_arg, operation))
@@ -21,10 +21,11 @@ if __name__ == "__main__":
     elif sys.argv[2] == "*":
         operation3 = calculator_1.mul(first_arg, sec_arg)
         print("{} * {} = {}".format(first_arg, sec_arg, operation))
-    
+
     elif sys.argv[2] == "/":
         operation4 = calculator_1.div(first_arg, sec_arg)
         print("{} / {} = {}".format(first_arg, sec_arg, operation))
-    
+
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
