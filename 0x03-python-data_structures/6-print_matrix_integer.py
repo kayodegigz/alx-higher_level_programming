b4ce18f97ec8bd9dffb0ddb1bd21936b2df53f77
kayodegigz <kayogunrinde@gmail.com>
2022-06-06 16:38:40 +0100

    print contents of a 3x3 matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        i = 0 #initialize i inside the loop again so the value is reset
         for r in row:
-            print("{:d}".format(r), end=" ")
-        print("\n")
+            row_len = len(row) - 1
+            if i < row_len:
+                print("{:d}".format(r), end=" ")
+            else:
+                print("{:d}".format(r))
+            i += 1
