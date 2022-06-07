0e8b44dcd4d5241f3bf8faebb40a818d1f2759f6
kayodegigz <kayogunrinde@gmail.com>
2022-06-07 17:25:31 +0100

    removes item at a particular index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list == []:
-        return None
-
-    if idx < 1 or idx > (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     i = 0
-    new_list = []
     while i < len(my_list):
         if i == idx:
             del my_list[i]
