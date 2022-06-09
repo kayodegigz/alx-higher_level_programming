d47427231828d7e7d5cfd007d878ac4f82f349f4
kayodegigz <kayogunrinde@gmail.com>
2022-06-09 17:56:18 +0100

    returns key with the highest int value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
-        for k,v in a_dictionary.items():
-            if v == max(a_dictionary.values):
+    if a_dictionary is not None:
+        for k, v in a_dictionary.items():
+            if v == max(a_dictionary.values()):
                 return k
-    return None
+    else:
+        return None
