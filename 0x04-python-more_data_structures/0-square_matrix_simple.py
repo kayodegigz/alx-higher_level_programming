62c857ff54f3e65b7ed6ace45fedb7ca8c519bbd
kayodegigz <kayogunrinde@gmail.com>
2022-06-09 15:27:17 +0100

    square items in a 2d matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for item in matrix:
-        new_row= []
+        new_row = []
         for num in item:
             new_row.append(num ** 2)
         new_matrix.append(new_row)
