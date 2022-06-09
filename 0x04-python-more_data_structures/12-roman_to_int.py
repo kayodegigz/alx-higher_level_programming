0c9e311fa260e1d7c4f72f1e4fcd13e8811069da
kayodegigz <kayogunrinde@gmail.com>
2022-06-09 21:54:46 +0100

    Change roman numerals to ints

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) == str or roman_string == None:
+        return 0
+
     # specifying values for all the single roman numerals
     r_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     i = 0
