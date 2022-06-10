f5ba58fc42c562e00f2a5d74a1b82e941ca07293
kayodegigz <kayogunrinde@gmail.com>
2022-06-10 08:28:25 +0100

    fixing dict altreation during iteration error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    a_list = []
     for k, v in a_dictionary.items():
         if v == value:
-            a_dictionary.pop(k)
-        else:
-            return a_dictionary
+            a_list.append(k)  # list of keys to be removed
+
+    for key in a_list:
+        a_dictionary.pop(key, None)
     return a_dictionary
