24e29577a64a457f480d38a4514a043e4f22573c
kayodegigz <kayogunrinde@gmail.com>
2022-06-10 08:14:00 +0100

    deletes dict keys by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,7 @@
 def complex_delete(a_dictionary, value):
     for k, v in a_dictionary.items():
         if v == value:
-            return a_dictionary.pop(k)
+            a_dictionary.pop(k)
         else:
             return a_dictionary
+    return a_dictionary
