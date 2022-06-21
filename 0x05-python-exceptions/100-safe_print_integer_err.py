cb5fa5a963bcc46a1db3a459ea44cdcac3efa6f5
kayodegigz <kayogunrinde@gmail.com>
2022-06-21 03:28:21 +0100

    handle and print exception error messages

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,10 +6,9 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as v:
-        print("Exception: {}".format(v.__str__), file = sys.stderr) 
+        print("Exception: {}".format(sys.exc_info()[1]), file = sys.stderr) 
         # above __str__ prints only err msg without the err type
         return False
     except TypeError as t:
-        print("Exception: {}".format(t.__str__), file = sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file = sys.stderr)
         return False
-
