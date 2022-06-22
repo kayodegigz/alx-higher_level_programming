c59f388d399a13e0d0b1f3fd6719de40d4625bb0
kayodegigz <kayogunrinde@gmail.com>
2022-06-23 00:53:33 +0100

    fixing more documentation errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 
-"""An empty class that defines a square"""
+
+"""An empty class that defines a square."""
 
 
 
 class Square:
-    """Empty class that does nothing"""
+    """Empty class that just defines a square class"""
     Pass
