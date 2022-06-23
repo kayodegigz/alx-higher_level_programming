5ad8a51ed3e2b48789678f2677dd517a3155493d
kayodegigz <kayogunrinde@gmail.com>
2022-06-23 01:54:41 +0100

    Instantiating a size variable in the square class and raising exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""A class that defines a square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Size of square is a hidden var and must be a positive int"""
+        self.size = __size
+
+        if typeof(__size) is not int:
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
