1433cc447b67cb8b123f4cf3dd172037ba3f7001
kayodegigz <kayogunrinde@gmail.com>
2022-06-23 02:32:39 +0100

    define a square with an area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""A class that defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """instantiating Size of square is a hidden var
+            and must be a positive int
+        Args:
+            size(int): The size of the square
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """The area of the square"""
+        return self.area
