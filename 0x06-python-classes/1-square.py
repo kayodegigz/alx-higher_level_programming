6efdfb2f7dcc21cf4832d6bb4e6a183a50d63697
kayodegigz <kayogunrinde@gmail.com>
2022-06-23 01:42:00 +0100

    creating a new class while initialising a private variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""A class that contains the size of a square"""
+
+
+class Square:
+    """represents a square"""
+    def __init__(self, size):
+        """Initialising a square
+        Args:
+        size(int)
+        """
+        self.__size = size
