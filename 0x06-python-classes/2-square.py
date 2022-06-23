d79211ec191974d8f4b2fea34f9d89ba119737fd
kayodegigz <kayogunrinde@gmail.com>
2022-06-23 02:09:18 +0100

    fixing errors in the initialisation method

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,14 +5,15 @@
 
 
 class Square:
+    """Represents a square"""
+
     def __init__(self, size=0):
         """instantiating Size of square is a hidden var
             and must be a positive int
         Args:
             size(int): The size of the square
         """
-        """if typeof(size) is not int:"""
-        if not isinstance(size, int):
+        if typeof(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
