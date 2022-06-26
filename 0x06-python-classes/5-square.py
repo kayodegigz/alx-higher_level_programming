b2eab9a33e18cf36ea683809f536e5ee18701ce1
kayodegigz <kayogunrinde@gmail.com>
2022-06-26 12:07:13 +0100

    printing squares in a class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+
+"""A class that defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """instantiating Size of square is a hidden var
+            and must be a positive int
+        Args:
+            size(int): The size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter that retrieves the value of the attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size of the attribute size equal to value
+        Args:
+            value(int): The value that the attribute will be set to
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """The area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#")
+            print()
