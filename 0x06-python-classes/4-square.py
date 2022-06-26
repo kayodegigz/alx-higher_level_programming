9bf5c70efb3ec1c64b70269c3900a4665dcc8c5d
kayodegigz <kayogunrinde@gmail.com>
2022-06-26 11:44:33 +0100

    removing unnecessary exceptions

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,13 +13,7 @@ class Square:
         Args:
             size(int): The size of the square
         """
-
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
