f1f8dd3aec1735a4f082b9eb438bc36546311a13
kayodegigz <kayogunrinde@gmail.com>
2022-06-26 15:16:56 +0100

    debugging errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,10 +49,7 @@ class Square:
         Args:
             value(int): the value position will be set to
         """
-        if (not(isinstance(value, tuple)) or
-                (value[0] < 0 and value[1] < 0) or 
-                len(value) != 2
-                or not(isinstance(value[0], int) and isinstance(value[1], int))):
+        if (not isinstance(value, tuple) or (value[0] < 0 or value[1] < 0) or len(value) != 2 or (not isinstance(value[0], int) or not isinstance(value[1], int))):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
