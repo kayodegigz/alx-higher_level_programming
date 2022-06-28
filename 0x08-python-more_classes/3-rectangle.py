8bd11674098044826b98fd96e7b54bcf99ab1bf2
kayodegigz <kayogunrinde@gmail.com>
2022-06-28 03:16:54 +0100

    printing out a rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""a module that contains a rectangle class"""
+
+
+class Rectangle:
+    """A rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """initialising values of instances
+        Args:
+            width(int): the width of the rectangle
+            height(int): the height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieving the value of the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setting the value of the width == value
+        Args:
+            value(int): value must be a positive int
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieving the value of the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting the value of the height == value
+        Args:
+            value(int): value must be an int
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width != 0 and self.__height != 0:
+            return (2 * (self.__width + self.__height))
+        return 0
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+        return"k"
