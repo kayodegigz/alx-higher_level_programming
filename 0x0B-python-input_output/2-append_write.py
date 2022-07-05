9a3bc2a0d2ad34995f35114a2d6109d290355589
kayodegigz <kayogunrinde@gmail.com>
2022-07-05 23:21:05 +0100

    appending text to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Module contains a func that appends text to a file"""
+
+
+def append_write(filename="", text=""):
+    """appends text to a file"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.append(text)
