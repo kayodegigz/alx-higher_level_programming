8d1a0dc7e389d00c7ac74d4a5628779be40f3f5c
kayodegigz <kayogunrinde@gmail.com>
2022-07-05 22:50:01 +0100

    fix documentation error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+
+"""reads all contents of a file"""
+
+
 def read_file(filename=""):
+    """function reads all the content of a file"""
     with open("filename", encoding="UTF8") as f:
         print(f.read())
