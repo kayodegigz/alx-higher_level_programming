0113cc06fca8d910c514d031485f22025e6b9309
kayodegigz <kayogunrinde@gmail.com>
2022-07-06 02:17:28 +0100

    add load and save json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module is a script that encodes python objects to json and saves"""
+
+
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').load_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+    from sys import argv
+
+    try:  # check if file exists by trying to load its contents
+        new_list = load_from_json_file(add_item.json)
+    except FileNotFoundError:  # if it doesn't exist init a fresh list
+        new_list = []
+    for arg in argv[1:]:
+        new_list.append(arg)
+    save_to_json(new_list, add_item.json)
