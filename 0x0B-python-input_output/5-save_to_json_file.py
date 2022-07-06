3987dc9cdc92e31cf60950b0cb851457b5b9499d
kayodegigz <kayogunrinde@gmail.com>
2022-07-06 01:40:49 +0100

    fixing doc errors

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,10 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes json obj into a txt file"""
+    """writes json obj into a txt file
+    Args:
+        @my_obj: the object to be encoded
+        @filename: name of the txt file
+    """
     with open(filename, "w") as f:
         json.dump(my_obj, f)
