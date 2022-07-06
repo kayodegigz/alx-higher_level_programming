c3e9a56ff54c814d6751e22395d5d3898712c671
kayodegigz <kayogunrinde@gmail.com>
2022-07-06 02:35:13 +0100

    add load and save json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,14 @@ import sys
 
 
 if __name__ == "__main__":
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:  # check if file exists by trying to load its contents
         new_list = load_from_json_file("add_item.json")
     except FileNotFoundError:  # if it doesn't exist init a fresh list
         new_list = []
     for arg in sys.argv[1:]:
-        new_list.append(argv[1:]
+        new_list.append(arg)
     save_to_json_file(new_list, "add_item.json")
