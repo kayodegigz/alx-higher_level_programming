6d80e3c531e5b76d105b8b90eadfd96493a7a51b
kayodegigz <kayogunrinde@gmail.com>
2022-07-09 23:40:49 +0100

    parsing lod files

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""module parses lines from stdin"""
+
+
 import sys
 
+
 def print_status():
+    """prints the status of log files as well as size"""
+
     counter = 0
     size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, 
-            "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    status_codes = {"200": 0, "301": 0, "400": 0,
+                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     for line in sys.stdin:
         counter += 1
         line_list = line.split()
@@ -22,13 +28,14 @@ def print_status():
                 if value != 0:
                     print("{}: {}".format(key, value))
             counter = 0
-        #counter += 1
+        # counter += 1
 
     if counter < 10:
         print("File size: {}".format(size))
         for key, value in status_codes.items():
             if value != 0:
                 print("{}: {}".format(key, value))
+
 
 if __name__ == "__main__":
     print_status()
