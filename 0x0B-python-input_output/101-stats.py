98071b8070b3bade4b104351a14066472968348b
kayodegigz <kayogunrinde@gmail.com>
2022-07-09 23:55:10 +0100

    parsing lod files

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,7 +13,7 @@ def print_status():
     status_codes = {"200": 0, "301": 0, "400": 0,
                     "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     for line in sys.stdin:
-        counter += 1
+        # counter += 1
         line_list = line.split()
         try:
             size += int(line_list[-1])
@@ -28,7 +28,7 @@ def print_status():
                 if value != 0:
                     print("{}: {:d}".format(key, value))
             counter = 0
-        # counter += 1
+        counter += 1
 
     if counter < 10:
         print("File size: {:d}".format(size))
