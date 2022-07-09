7006060684ff9ef12134ab8bbac34bbe53b53047
kayodegigz <kayogunrinde@gmail.com>
2022-07-10 00:50:36 +0100

    parsing logg files

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,6 +14,7 @@ def print_status():
                     "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
     for line in sys.stdin:
+        counter += 1
         line_list = line.split()
         try:
             size += int(line_list[-1])
@@ -22,15 +23,15 @@ def print_status():
         except Exception:
             continue
 
-        if counter == 9:
+        if counter == 10:
             print("File size: {}".format(size))
             for key, value in sorted(status_codes.items()):
                 if value != 0:
                     print("{}: {}".format(key, value))
             counter = 0
-        counter += 1
+        # counter += 1
 
-    if counter < 9:
+    if counter < 10:
         print("File size: {}".format(size))
         for key, value in sorted(status_codes.items()):
             if value != 0:
