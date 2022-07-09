8f74ac242f8944d79139c34eff41aae269e2c0ee
kayodegigz <kayogunrinde@gmail.com>
2022-07-10 00:45:51 +0100

    parsing logg files

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -12,11 +12,12 @@ def print_status():
     size = 0
     status_codes = {"200": 0, "301": 0, "400": 0,
                     "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+
     for line in sys.stdin:
         line_list = line.split()
         try:
             size += int(line_list[-1])
-            code = line[-2]
+            code = line_list[-2]
             status_codes[code] += 1
         except:
             continue
