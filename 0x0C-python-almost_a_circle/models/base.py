791379d9e75ce71d0236eb4fe9b4ee6cce550ab5
kayodegigz <kayogunrinde@gmail.com>
2022-07-12 22:45:57 +0100

    def base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module contains a class that defines a base"""
+
+
+class Base:
+    """A class that contains definitions for a base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
