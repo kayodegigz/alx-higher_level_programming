f1ba46300739fed167fe30fcaaf8f81cbfa9c612
kayodegigz <kayogunrinde@gmail.com>
2022-07-13 00:11:00 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,10 +7,10 @@ class Rectangle(Base):
     """Class defines rectangle prop. and inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initialising values of the instance"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
         Super().__init__(self, id)
 
