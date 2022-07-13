4c3992fcbee1dbbba8233e40f4fc271351b93006
kayodegigz <kayogunrinde@gmail.com>
2022-07-13 02:20:13 +0100

    fix docu errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,4 +76,4 @@ class Rectangle(Base):
     def __str__(self):
         """returns str representation of object"""
         return "[Rectangle] ({}) {}/{} - {}/{}"\
-                .format(self.id, self.__x, self.__y, self.__width, self.__height)
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
