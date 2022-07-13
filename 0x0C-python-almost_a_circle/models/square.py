0a57c4d99ef70f96fe3087b009aac5d47552d6e4
kayodegigz <kayogunrinde@gmail.com>
2022-07-13 16:52:41 +0100

    assigning width and height to same value

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,16 @@ class Square(Rectangle):
         """str format constructor"""
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
