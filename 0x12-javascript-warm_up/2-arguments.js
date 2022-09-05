5483eaaedf5bef09888c361a8df701b0fff40ab6
kayodegigz <kayogunrinde@gmail.com>
2022-09-05 16:00:02 -0700

    fix equality error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argLen = process.argv.length;
-console.log(argLen == 2 ? 'No argument' : argLen == 3 ? 'Argument found' : 'Arguments found');
+console.log(argLen === 2 ? 'No argument' : argLen === 3 ? 'Argument found' : 'Arguments found');
