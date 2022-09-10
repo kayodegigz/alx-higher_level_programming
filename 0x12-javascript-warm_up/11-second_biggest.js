f0104e8a85dbcc625eb699e8e42c840445899c64
kayodegigz <kayogunrinde@gmail.com>
2022-09-10 07:41:57 -0700

    fix semistandard error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2);
-let int_arr = [];
-if (arr.length < 2)
-  console.log(1);
-else
-{
-  for (i = 0; i < arr.length; i++)
-    int_arr[i] = parseInt(arr[i]);
-  const max_num = Math.max.apply(null, int_arr);
-  int_arr.splice(int_arr.indexOf(max_num), 1);
-  console.log(Math.max.apply(null, int_arr));
+const intArr = [];
+if (arr.length < 2) { console.log(0); } else {
+  for (let i = 0; i < arr.length; i++) { intArr[i] = parseInt(arr[i]); }
+  const maxNum = Math.max.apply(null, intArr);
+  intArr.splice(intArr.indexOf(maxNum), 1);
+  console.log(Math.max.apply(null, intArr));
 }
