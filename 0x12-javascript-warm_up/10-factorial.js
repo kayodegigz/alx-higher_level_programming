a11d1e8ed55989ce5041bd732a47427f19c1efff
kayodegigz <kayogunrinde@gmail.com>
2022-09-10 06:28:34 -0700

    fix docu errors

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-function factorial(n)
-{
-  if(isNaN(n) || n == 0 || n == 1)
-    return 1;
+function factorial (n) {
+  if (isNaN(n) || n === 0 || n === 1) { return 1; }
   return n * factorial(n - 1);
 }
 const n = parseInt(process.argv[2]);
