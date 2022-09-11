9ba497c06d649ded4d202178dc6a9e5eb5bcfc49
kayodegigz <kayogunrinde@gmail.com>
2022-09-11 08:02:14 -0700

    map a list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data');
+const list = require('./100-data').list;
 const nList = list.map(x => x * list.indexOf(x));
 console.log(list);
 console.log(nList);
