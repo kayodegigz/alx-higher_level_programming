5282d2ff5ef8941eede660efa9dfe6a8b2f65f5e
kayodegigz <kayogunrinde@gmail.com>
2022-09-11 07:21:36 -0700

    fix loop condition

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  last = (list.length) - 1;
+  let j = 0; //idx of new list 
+  for (let i = last; (i - j) > 0; i--) {
+    const temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+    j++;
+  }
+  return list;
+};
