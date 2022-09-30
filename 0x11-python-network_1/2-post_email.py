d902edf8a252637657d06e5d4ff18b52a2cb772b
kayodegigz <kayogunrinde@gmail.com>
2022-09-30 00:47:44 -0700

    add exception case

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,8 +13,8 @@
     """
 
 import sys
-import urllib.parse
 import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
@@ -24,5 +24,9 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode(value).encode("ascii")
     request = urllib.request.Request(url, data)
 
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    try:
+        with urllib.request.urlopen(request) as response:
+            if response:
+                print(response.read().decode("utf-8"))
+    except BaseException:
+        pass
