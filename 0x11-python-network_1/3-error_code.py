eb98d0f307de211023ee1171a70cbea13bb10082
kayodegigz <kayogunrinde@gmail.com>
2022-10-01 23:39:56 +0100

    trying out different doc style

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-"""
-
-    script that takes in a URL,
-    sends a request to the URL and displays
-    the body of the response (decoded in utf-8).
+""" A script that takes in a URL,
+-   sends a request to the URL and displays
+-   the body of the response (decoded in utf-8).
 """
 
 import urllib.request
