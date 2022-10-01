2e03035d4c6fa67f5b717bce643b31170153f56b
kayodegigz <kayogunrinde@gmail.com>
2022-10-02 00:08:27 +0100

    trying out different doc style

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,9 +8,8 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
 if __name__ == '__main__':
+    url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
             html = response.read()
