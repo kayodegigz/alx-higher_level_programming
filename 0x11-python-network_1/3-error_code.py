c5352085bcee95343e7a9653609e9d32a569e4bc
kayodegigz <kayogunrinde@gmail.com>
2022-10-01 23:02:32 +0100

    script that handles error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL,
+   sends a request to the URL and displays
+   the body of the response (decoded in utf-8).
+"""
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
+except urllib.error.HTTPError as err:
+    print("Error code: {}".format(err.code))
