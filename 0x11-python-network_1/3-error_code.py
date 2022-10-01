259bbe488d4155bdf477397853b6440273aec4cc
kayodegigz <kayogunrinde@gmail.com>
2022-10-01 23:15:34 +0100

    trying out different doc style

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 
 """
 
-    a Python script that takes in a URL,
+    script that takes in a URL,
     sends a request to the URL and displays
     the body of the response (decoded in utf-8).
 """
