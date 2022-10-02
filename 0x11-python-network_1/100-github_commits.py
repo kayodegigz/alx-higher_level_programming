fe4cf1f5e5a3193e55b062ccd13238e6145d7dfc
kayodegigz <kayogunrinde@gmail.com>
2022-10-02 03:32:10 +0100

    retrieve 10 most recent commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+     a Python script that takes 2 arguments in order to
+     list 10 commits (from the most recent to oldest)
+     of the repository “rails” by the user “rails”
+"""
+
+import requests
+import sys
+import requests.HTTPBasicAuth
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{:s}/{:s}/commits".format(
+        owner, repo)
+
+    response = requests.get(url)
+
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        name = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
