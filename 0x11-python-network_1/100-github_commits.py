dfb10c176720967c80ac505b151c9cea8c917846
kayodegigz <kayogunrinde@gmail.com>
2022-10-02 03:34:16 +0100

    retrieve 10 most recent commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,7 @@ import requests.HTTPBasicAuth
 if __name__ == '__main__':
     repo = sys.argv[1]
     owner = sys.argv[2]
+
     url = "https://api.github.com/repos/{:s}/{:s}/commits".format(
         owner, repo)
 
