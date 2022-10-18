84809bf5858ce5c246c5d7cfe991ddf0c7f978b9
kayodegigz <kayogunrinde@gmail.com>
2022-10-18 23:25:28 +0100

    fix semistandard errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,18 +5,18 @@ request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const response_body = JSON.parse(body);
-    const res = response_body.results;
+    const responseBody = JSON.parse(body);
+    const res = responseBody.results;
     let count = 0;
     for (let i = 0; i < res.length; i++) {
       const chars = res[i].characters;
       for (let j = 0; j < chars.length; j++) {
-        const check_id18 = chars[j].endsWith('18/');
-	if (check_id18) {
+        const checkId18 = chars[j].endsWith('18/');
+        if (checkId18) {
           count++;
-	}
+        }
       }
     }
-  console.log(count);
+    console.log(count);
   }
 });
