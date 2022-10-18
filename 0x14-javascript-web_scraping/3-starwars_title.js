1486c98393685a72ae8092bcd3e333a143a820eb
kayodegigz <kayogunrinde@gmail.com>
2022-10-18 22:56:13 +0100

    requests the star wars api and gets a movie title based on the movie id

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
