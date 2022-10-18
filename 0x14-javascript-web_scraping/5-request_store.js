b97b3e4f2face92c2394f8cfbdd44f7bcbfb6753
kayodegigz <kayogunrinde@gmail.com>
2022-10-18 23:42:14 +0100

    get content of a response and save the body to a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
